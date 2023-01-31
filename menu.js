2a5fc201739d0a82dafae5253c7e183c12e342da
Roland-Ntwali <rolandntwali@gmail.com>
2023-01-31 21:22:03 +0200

    Add styling for menu

--- menu.js
+++ menu.js
@@ -1,10 +1,19 @@
 const menu_button = document.getElementById("menu_btn");
 const close_menu = document.getElementById("close_menu");
+const hide_menu = document.getElementById("hideb")
 
 menu_button.addEventListener("click", () => {
   mobile_menu.classList.add("show");
+  menu_button.style.display = 'none';
 });
 
 close_menu.addEventListener("click", () => {
   mobile_menu.classList.remove("show");
+  menu_button.classList.remove("mobile_menu");
+  menu_btn.style.display = 'flex';
+});
+
+hide_menu.addEventListener("click", () => {
+  mobile_menu.classList.remove("show");
+  menu_btn.style.display = 'flex';
 });

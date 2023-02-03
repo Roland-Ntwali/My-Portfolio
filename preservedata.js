e7ab5890d9bf7638dad39ad6b2a98e8ea9e5f70f
Roland-Ntwali <rolandntwali@gmail.com>
2023-02-04 01:29:29 +0200

    Fix linter error

--- preservedata.js
+++ preservedata.js
@@ -1,27 +1,26 @@
 // Store data in local storage
 function storeData(data) {
-    localStorage.setItem("storedData", JSON.stringify(data));
+  localStorage.setItem('storedData', JSON.stringify(data));
+}
+
+// Retrieve data from local storage
+function retrieveData() {
+  return JSON.parse(localStorage.getItem('storedData')) || {};
+}
+const form = document.getElementById('form');
+const inputs = form.querySelectorAll('input');
+
+// Autofill form fields with stored data
+const storedData = retrieveData();
+inputs.forEach((input) => {
+  if (storedData[input.name]) {
+    input.value = storedData[input.name];
   }
-  
-  // Retrieve data from local storage
-  function retrieveData() {
-    return JSON.parse(localStorage.getItem("storedData")) || {};
-  }
-  
-  const inputs = form.querySelectorAll("input");
-  
-  // Autofill form fields with stored data
-  const storedData = retrieveData();
-  inputs.forEach((input) => {
-    if (storedData[input.name]) {
-      input.value = storedData[input.name];
-    }
-  });
-  
-  // Update stored data when input fields change
-  form.addEventListener("input", function (event) {
-    const input = event.target;
-    storedData[input.name] = input.value;
-    storeData(storedData);
-  });
-  
+});
+
+// Update stored data when input fields change
+form.addEventListener('input', (event) => {
+  const input = event.target;
+  storedData[input.name] = input.value;
+  storeData(storedData);
+});

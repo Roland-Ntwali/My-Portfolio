e7b9eee73e19cbeb49a719d87ce8b8810f2941b1
Roland-Ntwali <rolandntwali@gmail.com>
2023-02-03 13:27:03 +0200

    Change in styling

--- popup/popup.js
+++ popup/popup.js
@@ -31,7 +31,7 @@ const projects = [
     title: 'TONIC 4',
     prof: ['Canopy', 'back end Dev', '2015'],
     src: 'images/SnapshootPortfolio.png',
-    description: ["Roland"],
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe."],
     technologies: ['html', 'css', 'javascript', 'Github', 'Ruby', 'Bootstrap'],
     seeLiveLink: '#',
     seeSourceLink: '#',

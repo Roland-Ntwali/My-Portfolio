b7c09087af7c1bef4c968bad5be710cb66acb73e
Roland-Ntwali <rolandntwali@gmail.com>
2023-02-04 00:30:38 +0200

    Add preserve data

---
+++ preserve-data.js
@@ -0,0 +1,27 @@
+// Store data in local storage
+function storeData(data) {
+    localStorage.setItem("storedData", JSON.stringify(data));
+  }
+  
+  // Retrieve data from local storage
+  function retrieveData() {
+    return JSON.parse(localStorage.getItem("storedData")) || {};
+  }
+  
+  const inputs = form.querySelectorAll("input");
+  
+  // Autofill form fields with stored data
+  const storedData = retrieveData();
+  inputs.forEach((input) => {
+    if (storedData[input.name]) {
+      input.value = storedData[input.name];
+    }
+  });
+  
+  // Update stored data when input fields change
+  form.addEventListener("input", function (event) {
+    const input = event.target;
+    storedData[input.name] = input.value;
+    storeData(storedData);
+  });
+  
